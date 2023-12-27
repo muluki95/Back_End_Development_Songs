193f44ddc29d16b1651aeef3766f2528a33344d4
muluki95 <145924235+muluki95@users.noreply.github.com>
2023-12-27 12:34:24 -0600

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,10 +4,10 @@ echo " Setting up Capstone Environment"
 echo "****************************************"
 
 sudo apt-get update
-echo "Installing Python 3.8 Virtual Environment"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
+echo "Installing Python 3.10 Virtual Environment"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.10-venv
 
-echo "Making Python 3.8 the default..."
+echo "Making Python 3.10 the default..."
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
 
